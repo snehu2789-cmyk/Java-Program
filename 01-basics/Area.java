4b6b158e8ce5a7d698fc241b6c1bdfc7943bc93d
Sneha Das <snehu2789@gmail.com>
2026-08-20 21:03:40 +0545

    Rename main method parameter from 'aids' to 'args'

--- 01-basics/Area.java
+++ 01-basics/Area.java
@@ -1,7 +1,7 @@
 import java.util.*; 
 class AC 
 { 
-public static void main(String[] aids) 
+public static void main(String[] args) 
 { 
 System.out.println("Enter the radius"); 
 Scanner kb=new Scanner(System.in); 
