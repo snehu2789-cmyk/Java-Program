2b162b8d167804c61fbbda72be8a0d300ee90073
snehu2789-cmyk <snehu2789@gmail.com>
2026-09-03 07:54:19 +0545

    Finding ceiling of array

---
+++ 07-Binary-Search/Ceiling.java
@@ -0,0 +1,23 @@
+public class Ceiling{
+    public static void main(String[] args){
+        int[] arr = {2, 3, 5, 7, 8, 9 ,12, 14 ,16};
+        int target = 12;
+        int ans = ceilingSearch(arr, target);
+        System.out.println(ans);
+    }
+    static int ceilingSearch(int[] arr, int target){
+        int start  = 0;
+        int end = arr.length-1;
+        while (start <= end){
+            int mid = start + (end-start)/2;
+            if(target > arr[mid]){
+                start = mid+1;
+            } else if (target < arr[mid]){
+                end = mid - 1;
+            }else {
+                return  arr[mid];
+            }
+        }
+        return  arr[start];
+    }
+}
